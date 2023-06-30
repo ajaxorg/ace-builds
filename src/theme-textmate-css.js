903fec20031df226c6f9897cd0bf93e2c21ce16b
nightwing <amirjanyan@gmail.com>
2023-06-30 18:50:08 +0400

    remove broken theme files and add tests

--- src/theme-textmate-css.js
+++
@@ -1,9 +0,0 @@
-
-;                (function() {
-                    window.require(["ace/theme/textmate-css"], function(m) {
-                        if (typeof module == "object" && typeof exports == "object" && module) {
-                            module.exports = m;
-                        }
-                    });
-                })();
-            
